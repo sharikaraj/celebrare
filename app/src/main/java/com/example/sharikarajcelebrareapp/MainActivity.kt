347d498e231203e46ea25ca786b65ec875ee2750
Sharika <rajsharika6200@gmail.com>
2024-02-16 16:34:11 +0530

    not completed

---
+++ app/src/main/java/com/example/sharikarajcelebrareapp/MainActivity.kt
@@ -0,0 +1,128 @@
+package com.example.sharikarajcelebrareapp
+
+import android.graphics.Color
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.SeekBar
+import androidx.appcompat.app.AppCompatActivity
+import yuku.ambilwarna.AmbilWarnaDialog
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var writingView: WritingView
+    private lateinit var fontSize: SeekBar
+    private var defaultColor: Int = Color.WHITE
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        // writingView = WritingView(this)
+
+        val editText = findViewById<EditText>(R.id.editText)
+        writingView = findViewById<WritingView>(R.id.writingView)
+        //  val changeSize = findViewById<Button>(R.id.btnChangeSize)
+        val changeColor = findViewById<Button>(R.id.btnChangeColor)
+        val addText = findViewById<Button>(R.id.addText)
+        fontSize = findViewById(R.id.fontSizeProgress)
+
+
+        editText.setOnClickListener {
+
+        }
+
+        editText.setOnEditorActionListener { _, _, _ ->
+            val inputText = editText.text.toString()
+            writingView.setText(inputText)
+            true
+        }
+
+        // changeSize.setOnClickListener { }
+
+        changeColor.setOnClickListener {
+
+            openColorPicker()
+
+        }
+
+        addText.setOnClickListener {
+
+            val newText = editText.text.toString()
+            val x = 100f
+            val y = 100f
+            val textColor = Color.BLUE
+            writingView.addText(newText, x, y, textColor)
+            editText.text.clear()
+
+
+           // val newTextIndex = writingView.textList.size - 1
+           // writingView.textList[newTextIndex].setOnClickListener {
+             //  openColorPicker(newTextIndex)}
+
+            fontSize.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+                override fun onProgressChanged(
+                    seekBar: SeekBar?,
+                    progress: Int,
+                    fromUser: Boolean
+                ) {
+                    val newTextSize = progress.toFloat()
+                    writingView.setTextSize(newTextSize)
+                }
+
+                override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+                }
+
+                override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+                }
+
+
+            })
+
+        }
+
+      /*  private fun openColorPicker() {
+
+            val ambilWarnaDialog = AmbilWarnaDialog(this, defaultColor,
+                object : AmbilWarnaDialog.OnAmbilWarnaListener {
+                    override fun onOk(dialog: AmbilWarnaDialog?, color: Int) {
+
+
+                        //val defaultColor: Int = Color.WHITE
+
+                        writingView.setTextColor(0, color)
+
+                    }
+
+                    override fun onCancel(dialog: AmbilWarnaDialog?) {
+
+                    }
+                }
+            )
+            ambilWarnaDialog.show()
+        } */
+
+
+    }
+
+    private fun openColorPicker() {
+        val ambilWarnaDialog = AmbilWarnaDialog(this, defaultColor,
+            object : AmbilWarnaDialog.OnAmbilWarnaListener {
+                override fun onOk(dialog: AmbilWarnaDialog?, color: Int) {
+
+
+                    //val defaultColor: Int = Color.WHITE
+
+                    writingView.setTextColor(0, color)
+
+                }
+
+                override fun onCancel(dialog: AmbilWarnaDialog?) {
+
+                }
+            }
+        )
+        ambilWarnaDialog.show()
+    }
+}
---
+++ app/src/main/java/com/example/sharikarajcelebrareapp/WritingView.kt
@@ -0,0 +1,125 @@
+package com.example.sharikarajcelebrareapp
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.PointF
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+import yuku.ambilwarna.AmbilWarnaDialog
+
+class WritingView (context: Context, attrs: AttributeSet?) : View(context, attrs) {
+
+    private val paint = Paint().apply {
+        color = Color.BLACK
+        textSize = 50f
+    }
+
+    private val textList = mutableListOf<TextData>()
+    private var selectedTextIndex: Int? = null
+
+   // private var text = ""
+  //  private var textPosition = PointF(100f, 100f)
+    //private var touchOffset = PointF()
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        textList.forEach { textData ->
+            paint.color = textData.color
+            canvas.drawText(textData.text, textData.x, textData.y, paint)
+        }
+
+
+    }
+
+
+
+
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+            when (event.action) {
+          MotionEvent.ACTION_DOWN -> {
+              val touchX = event.x
+              val touchY = event.y
+  
+              for (textData in textList) {
+                  if (touchX >= textData.x && touchX <= textData.x + paint.measureText(textData.text)
+                      && touchY >= textData.y - paint.textSize && touchY <= textData.y
+                  ) {
+                      textData.touchOffsetX = touchX - textData.x
+                      textData.touchOffsetY = touchY - textData.y
+                      textData.isBeingDragged = true
+                      return true
+                  }
+              }
+          }
+          MotionEvent.ACTION_MOVE -> {
+              val touchX = event.x
+              val touchY = event.y
+              textList.forEach { textData ->
+                  if (textData.isBeingDragged) {
+                      textData.x = touchX - textData.touchOffsetX
+                      textData.y = touchY - textData.touchOffsetY
+                      invalidate()
+                      return true
+  
+                  }
+              }
+          }
+  
+          MotionEvent.ACTION_UP -> {
+              textList.forEach { it.isBeingDragged = false }
+          }
+      }
+  
+  
+  
+  
+      return super.onTouchEvent(event)
+  }
+
+
+      fun setText(newText: String,  color: Int = Color.BLACK) {
+        textList.clear()
+        addText(newText, 100f, 100f, color)
+
+
+    }
+
+
+
+    fun addText(newText: String, x: Float, y: Float,color: Int) {
+        textList.add(TextData(newText, x, y, color))
+        invalidate()
+    }
+
+    fun setTextSize(newTextSize: Float) {
+        paint.textSize = newTextSize
+        invalidate()
+
+    }
+
+    fun setTextColor(textIndex: Int, color: Int) {
+
+        if (textIndex >= 0 && textIndex < textList.size) {
+            textList[textIndex].color = color
+            paint.color = color
+            invalidate()
+        }
+    }
+
+
+    private data class TextData(
+        val text: String,
+        var x: Float,
+        var y: Float,
+        var color: Int,
+        var touchOffsetX: Float = 0f,
+        var touchOffsetY: Float = 0f,
+        var isBeingDragged: Boolean = false
+    )
+    }
+
+
